e882f7e597ffed7059efe77f21f891f2ee81692a
Devin Shoemaker <devinshoe@gmail.com>
2024-01-17 11:49:36 -0600

    chore: improve ESLint rules

--- src/db.ts
+++ src/db.ts
@@ -5,11 +5,14 @@ const prismaClientSingleton = () => {
 };
 
 declare global {
+  // eslint-disable-next-line no-unused-vars
   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
+// eslint-disable-next-line no-undef
 const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
+// eslint-disable-next-line no-undef
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
